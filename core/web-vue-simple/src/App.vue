6b521fa358b1c00ca8a812a70403d90976a5bd33
李雨锋 <liyufeng@gz-yibo.com>
2023-03-08 22:20:11 +0800

    🔖 准备就绪...

--- core/web-vue-simple/src/App.vue
+++ core/web-vue-simple/src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/logo.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
@@ -24,6 +24,6 @@ import HelloWorld from "./components/HelloWorld.vue";
   filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 2em #2e72dfaa);
 }
 </style>
